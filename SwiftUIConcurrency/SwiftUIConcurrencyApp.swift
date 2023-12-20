e9250ef0ba8db278176132fc7876bece6c9afe8b
Mackley <mackleymagalhaess@hotmail.com>
2023-12-20 11:34:13 -0300

    Initial Commit

---
+++ SwiftUIConcurrency/SwiftUIConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIConcurrencyApp.swift
+//  SwiftUIConcurrency
+//
+//  Created by Mackley Magalhães da Silva on 20/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
